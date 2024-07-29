90db4852ea918a38d171be789c56de987c1e5116
Imtroll05 <imtroll05@gmail.com>
2024-07-29 15:16:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7125635368:AAGDdSpnCXPTzShPYNt8TX-Izb4j-FPdGRk")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("imtroll_bot")
 
-  #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+  #Enter your admin id here Get it from  by typing /info
+ADMIN_IDS=("1286215779")
 
 
 
